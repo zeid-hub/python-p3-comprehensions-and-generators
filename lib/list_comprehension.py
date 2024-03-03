48ccd5d7f92865d2377da2ba984633e0365dc152
zeid <zeidhassan4@gmail.com>
2024-03-03 06:56:26 +0300

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    print([num for num in num_list if num %2 ==0])
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
 
 def make_exclamation(sentence_list):
-    pass
+    print([sentence + "!" for sentence in sentence_list])
+    
+make_exclamation(["Hello", "How are you?"])
